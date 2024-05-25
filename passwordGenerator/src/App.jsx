fd8f1ad027a7dd103b7730aa7fdb398813dc2de2
ashutosh0640 <ashulook1013@gmail.com>
2024-05-25 07:17:10 +0530

    passwordGenerator updated

---
+++ passwordGenerator/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <h1 className='text-4xl text-center text-white'>Password Generator</h1>
+      
+    </>
+  )
+}
+
+export default App
